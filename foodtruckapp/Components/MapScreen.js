610c57256995fc5f0277b16484a29c16f229473b
Rohan Kumar <rohankumar0513@gmail.com>
2024-02-01 23:51:08 -0600

    final design changes

--- foodtruckapp/Components/MapScreen.js
+++ foodtruckapp/Components/MapScreen.js
@@ -160,34 +160,34 @@ export default function MapScreen({ navigation }) {
         // Call any action
         setTimeout(() => {
           const dbRef = ref(db);
-          get(child(dbRef, `users/82LyYqZ73TZ2XUZizHj9piktknm1/data`)).then((snapshot) => {
-              if (snapshot.exists()) {
-                  const data = snapshot.val();
-                  const trucks = Object.keys(data).map(key => ({
-                      ...data[key]
-                  }))
-                  const sortedTruckData = trucks.sort((a, b) => a.name.localeCompare(b.name));
-                  setTruckData([]);
-                  Geocoder.init("AIzaSyBzBg_8V451VUSWuujZtTcn03gHJBok97A");
-                  for (let i = 0; i < sortedTruckData.length; i++) {
-                    const startDate = new Date(sortedTruckData[i].startTime);
-                    const endDate = new Date(sortedTruckData[i].endTime);
-                    const currentDate = new Date(Date.now());
-                    if (currentDate >= startDate && currentDate <= endDate) {
-                      Geocoder.from(sortedTruckData[i].location)
-                      .then(json => {
-                        var location = json.results[0].geometry.location;
-                        sortedTruckData[i].coords = location;
-                        setTruckData((prevData) => [...prevData, sortedTruckData[i]]);
-                      })
-                      .catch(error => console.warn(error));
-                    }
-                  }
-              } else {
-                  console.log("No data available");
+          get(child(dbRef, `users`)).then((dataSnapshot) => {
+            if (dataSnapshot.exists()) {
+              const users = Object.keys(dataSnapshot.val());
+              for (var j = 0; j < users.length; j++) {
+                const data = dataSnapshot.val()[users[j]]["data"];
+                console.log(data);
+                const trucks = Object.keys(data).map(key => ({
+                    ...data[key]
+                }))
+                const sortedTruckData = trucks.sort((a, b) => a.name.localeCompare(b.name));
+                setTruckData([]);
+                Geocoder.init("AIzaSyBzBg_8V451VUSWuujZtTcn03gHJBok97A");
+                for (let i = 0; i < sortedTruckData.length; i++) {
+                  const startDate = new Date(sortedTruckData[i].startTime);
+                  const endDate = new Date(sortedTruckData[i].endTime);
+                  const currentDate = new Date(Date.now());
+                  if (currentDate >= startDate && currentDate <= endDate) {
+                    Geocoder.from(sortedTruckData[i].location)
+                    .then(json => {
+                      var location = json.results[0].geometry.location;
+                      sortedTruckData[i].coords = location;
+                      setTruckData((prevData) => [...prevData, sortedTruckData[i]]);
+                    })
+                    .catch(error => console.warn(error));
+                  }               
+                }
               }
-          }).catch((error) => {
-              console.error(error);
+            }
           });
           
           setLoading(false);
